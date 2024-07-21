e598fcbeb088cb08ec485b6c6c4b3af03ade47c9
Kio <kio.thedev@gmail.com>
2024-07-21 18:42:24 -0400

    feat: :sparkles: Automatically reconnect to websocket if kicked off!

--- src/helpers/websocketHelper.ts
+++ src/helpers/websocketHelper.ts
@@ -20,130 +20,138 @@ export class CardboardWebsocket {
         private readonly cardboard: CardboardClient,
         public readonly websocketOptions: WebsocketOptions,
     ) {
-        // Verify Validity of Token before trying to jam it into the websocket builder.
-        this.cardboard.logger.debug("[WS] authenticating...");
-        misskeyRequest(this.cardboard, "i").then(() => {
-            const websocketBuilder = new URL(
-                `/streaming?i=${this.cardboard.accessToken}`,
-                `wss://${this.cardboard.instance}`,
-            );
+        const _connect = () => {
+            // Verify Validity of Token before trying to jam it into the websocket builder.
+            this.cardboard.logger.debug("[WS] authenticating...");
+            misskeyRequest(this.cardboard, "i").then(() => {
+                const websocketBuilder = new URL(
+                    `/streaming?i=${this.cardboard.accessToken}`,
+                    `wss://${this.cardboard.instance}`,
+                );
 
-            this.cardboard.logger.debug("[WS] Starting Connection...");
-            this.websocket = new WebSocket(websocketBuilder, {
-                // @ts-ignore headers is sanctioned by Bun.
-                headers: {
-                    "user-agent": `Cardboard/${pkg.version} (Misskey Bot; https://cardboard.kitsu.life/)`,
-                    "content-type": "application/json; charset=utf8",
-                    accept: "application/json",
-                    "accept-encoding": "gzip, deflate, br",
-                },
-            });
-            this.websocket.addEventListener("error", console.error);
+                this.cardboard.logger.debug("[WS] Starting Connection...");
+                this.websocket = new WebSocket(websocketBuilder, {
+                    // @ts-ignore headers is sanctioned by Bun.
+                    headers: {
+                        "user-agent": `Cardboard/${pkg.version} (Misskey Bot; https://cardboard.kitsu.life/)`,
+                        "content-type": "application/json; charset=utf8",
+                        accept: "application/json",
+                        "accept-encoding": "gzip, deflate, br",
+                    },
+                });
 
-            this.websocket.addEventListener("message", (rawData) => {
-                const data = JSON.parse(rawData.data);
-                cardboard.logger.verbose(`[WS] <- -- ${rawData.data}`);
-                //TODO: Not sure what else to add here.
-                // Will come back in time.
-                // Default catchall will service the rest in the meantime.
-                if (data.type === "channel") {
-                    switch (data.body.type) {
-                        case "note": {
-                            this.cardboard.logger.debug(
-                                `[WS] note ${data.body.body.id} received.`,
-                            );
-                            this.cardboard.emit(
-                                "note",
-                                new Note(this.cardboard, data.body.body),
-                            );
-                            break;
-                        }
-                        case "mention": {
-                            this.cardboard.logger.debug(
-                                "[WS] mention received.",
-                            );
-                            this.cardboard.emit(
-                                "mention",
-                                new Note(this.cardboard, data.body.body),
-                            );
-                            break;
-                        }
-                        default: {
-                            //This ia a catchall operator that will call out events as misskey says them- and they're only parsed, not wrapped @ the Note Factory.
-                            // Not sure I fully like that tbh.
-                            this.cardboard.logger.debug(
-                                `[WS] unknown type received! - ${data.body.type}`,
-                            );
-                            this.cardboard.emit(data.body.type, data.body.body);
+                // TODO: This should be a proper handler.
+                // this.websocket.addEventListener("error", this.cardboard.logger.error);
+
+                this.websocket.addEventListener("message", (rawData) => {
+                    const data = JSON.parse(rawData.data);
+                    cardboard.logger.verbose(`[WS] <- -- ${rawData.data}`);
+                    //TODO: Not sure what else to add here.
+                    // Will come back in time.
+                    // Default catchall will service the rest in the meantime.
+                    if (data.type === "channel") {
+                        switch (data.body.type) {
+                            case "note": {
+                                this.cardboard.logger.debug(
+                                    `[WS] note ${data.body.body.id} received.`,
+                                );
+                                this.cardboard.emit(
+                                    "note",
+                                    new Note(this.cardboard, data.body.body),
+                                );
+                                break;
+                            }
+                            case "mention": {
+                                this.cardboard.logger.debug(
+                                    "[WS] mention received.",
+                                );
+                                this.cardboard.emit(
+                                    "mention",
+                                    new Note(this.cardboard, data.body.body),
+                                );
+                                break;
+                            }
+                            default: {
+                                //This ia a catchall operator that will call out events as misskey says them- and they're only parsed, not wrapped @ the Note Factory.
+                                // Not sure I fully like that tbh.
+                                this.cardboard.logger.debug(
+                                    `[WS] unknown type received! - ${data.body.type}`,
+                                );
+                                this.cardboard.emit(
+                                    data.body.type,
+                                    data.body.body,
+                                );
+                            }
                         }
                     }
-                }
-            });
+                });
 
-            this.websocket.addEventListener("close", () => {
-                //TODO: Reconnect to the websocket.
-                this.cardboard.logger.warn("[WS] Websocket closed.");
-            });
+                this.websocket.addEventListener("close", () => {
+                    //TODO: Reconnect to the websocket.
+                    this.cardboard.logger.warn("[WS] Websocket closed.");
+                    _connect();
+                });
 
-            this.websocket.addEventListener("open", () => {
-                this.cardboard.logger.debug("[WS] connected!");
-                this.cardboard.logger.verbose(
-                    `[WS] -> -- ${JSON.stringify({
-                        type: "connect",
-                        body: {
-                            channel: "main",
-                            id: this.id++,
-                        },
-                    })}`,
-                );
-                this.websocket.send(
-                    JSON.stringify({
-                        type: "connect",
-                        body: {
-                            channel: "main",
-                            id: this.id++,
-                        },
-                    }),
-                );
-                this.cardboard.logger.verbose(
-                    `[WS] -> -- ${JSON.stringify({
-                        type: "connect",
-                        body: {
-                            channel: this.websocketOptions.TimelineType,
-                            id: this.id++,
-                            params: {
-                                withRenotes:
-                                    !!this.websocketOptions.withRenotes,
-                                withReplies:
-                                    !!this.websocketOptions.withReplies,
-                                withBots: !!this.websocketOptions.withBots,
+                this.websocket.addEventListener("open", () => {
+                    this.cardboard.logger.debug("[WS] connected!");
+                    this.cardboard.logger.verbose(
+                        `[WS] -> -- ${JSON.stringify({
+                            type: "connect",
+                            body: {
+                                channel: "main",
+                                id: this.id++,
                             },
-                        },
-                    })}`,
-                );
-                this.cardboard.logger.debug("[WS] channel opened: main");
-                this.websocket.send(
-                    `[WS] -> -- ${JSON.stringify({
-                        type: "connect",
-                        body: {
-                            channel: this.websocketOptions.TimelineType,
-                            id: this.id++,
-                            params: {
-                                withRenotes:
-                                    !!this.websocketOptions.withRenotes,
-                                withReplies:
-                                    !!this.websocketOptions.withReplies,
-                                withBots: !!this.websocketOptions.withBots,
+                        })}`,
+                    );
+                    this.websocket.send(
+                        JSON.stringify({
+                            type: "connect",
+                            body: {
+                                channel: "main",
+                                id: this.id++,
                             },
-                        },
-                    })}`,
-                );
-                this.cardboard.logger.debug(
-                    `[WS] channel opened: ${this.websocketOptions.TimelineType}`,
-                );
-                this.cardboard.emit("ready");
+                        }),
+                    );
+                    this.cardboard.logger.verbose(
+                        `[WS] -> -- ${JSON.stringify({
+                            type: "connect",
+                            body: {
+                                channel: this.websocketOptions.TimelineType,
+                                id: this.id++,
+                                params: {
+                                    withRenotes:
+                                        !!this.websocketOptions.withRenotes,
+                                    withReplies:
+                                        !!this.websocketOptions.withReplies,
+                                    withBots: !!this.websocketOptions.withBots,
+                                },
+                            },
+                        })}`,
+                    );
+                    this.cardboard.logger.debug("[WS] channel opened: main");
+                    this.websocket.send(
+                        `[WS] -> -- ${JSON.stringify({
+                            type: "connect",
+                            body: {
+                                channel: this.websocketOptions.TimelineType,
+                                id: this.id++,
+                                params: {
+                                    withRenotes:
+                                        !!this.websocketOptions.withRenotes,
+                                    withReplies:
+                                        !!this.websocketOptions.withReplies,
+                                    withBots: !!this.websocketOptions.withBots,
+                                },
+                            },
+                        })}`,
+                    );
+                    this.cardboard.logger.debug(
+                        `[WS] channel opened: ${this.websocketOptions.TimelineType}`,
+                    );
+                    this.cardboard.emit("ready");
+                });
             });
-        });
+        };
     }
 
     private id = 0;
