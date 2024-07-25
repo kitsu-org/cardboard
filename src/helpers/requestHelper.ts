c3dcf8e1a7e55863eeb39b4eb80062fe0982acc7
Kio <kio.thedev@gmail.com>
2024-07-25 08:15:22 -0400

    refactor: :recycle: Prevent non-json responses from being accessed

--- src/helpers/requestHelper.ts
+++ src/helpers/requestHelper.ts
@@ -1,8 +1,16 @@
 import type { CardboardClient } from "..";
 import pkg from "../../package.json" assert { type: "json" };
-import { AuthenticationError, PermissionDeniedError } from "../types/error";
+import {
+    AuthenticationError,
+    NotValidJsonError,
+    PermissionDeniedError,
+} from "../types/error";
 
 export const misskeyRequest = async (
+    p0: string,
+    p1: string,
+    p2: string,
+    p3: { option: string },
     cardboard: CardboardClient,
     path: string,
     options?: Record<string, unknown>,
@@ -30,32 +38,32 @@ export const misskeyRequest = async (
         ) {
             return await response.json();
         }
-    } else {
-        switch (response.status) {
-            case 401:
-                throw new AuthenticationError();
-            case 403:
-                throw new PermissionDeniedError();
-            default: {
-                if (
-                    !response.headers
-                        .get("content-type")
-                        ?.includes("application/json")
-                ) {
-                    throw new Error(`Request Error: ${response.status}`);
-                }
-                const errorInfo = (await response.json()) as {
-                    error: {
-                        code: string;
-                        message: string;
-                        uuid: string;
-                    };
-                };
-                throw {
-                    name: errorInfo.error.code,
-                    message: errorInfo.error.message,
-                };
+        throw NotValidJsonError;
+    }
+    switch (response.status) {
+        case 401:
+            throw new AuthenticationError();
+        case 403:
+            throw new PermissionDeniedError();
+        default: {
+            if (
+                !response.headers
+                    .get("content-type")
+                    ?.includes("application/json")
+            ) {
+                throw new Error(`Request Error: ${response.status}`);
             }
+            const errorInfo = (await response.json()) as {
+                error: {
+                    code: string;
+                    message: string;
+                    uuid: string;
+                };
+            };
+            throw {
+                name: errorInfo.error.code,
+                message: errorInfo.error.message,
+            };
         }
     }
 };
